b8c46096cb40105b19ca37d42cc58f2a5174f30e
Raghav Dinesh <Raghav.Dinesh@ibm.com>
2025-08-06 17:44:58 +0530

    chore: remove root-level tools and configs for index-only structure

--- tools/init/init_linter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Auto-format Python using black, then run flake8
-
-echo "[*] Formatting with black..."
-black ./projects ./tools
-
-echo "[*] Running flake8..."
-flake8 ./projects ./tools
-
